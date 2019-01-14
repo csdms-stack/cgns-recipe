f5d23e343722ebd0703adf66d1e196d3feea0941
Mark Piper <mark.piper@colorado.edu>
2019-01-14 15:54:46 -0700

    Update CMake configuration
    This was the result of lots of experimenting, with help from `ccmake`.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,25 +1,15 @@
 #! /usr/bin/env bash
 
-export HDF_LIB=$BUILD_PREFIX/lib/libhdf5.so
-export HDF_INC=$BUILD_PREFIX/include
-export ZLIB_LIB=$BUILD_PREFIX/lib/libz.so
-
 mkdir _build && cd _build
 cmake .. \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCMAKE_C_FLAGS:STRING=-D_LARGEFILE64_SOURCE \
-    -DCGNS_ENABLE_HDF5=YES \
-    -DHDF5_INCLUDE_PATH=$HDF_INC \
-    -DHDF5_LIBRARY=$HDF_LIB \
-    -DHDF5_NEED_SZIP=NO \
-    -DHDF5_NEED_ZLIB=YES \
-    -DZLIB_LIBRARY=$ZLIB_LIB \
-    -DCGNS_ENABLE_FORTRAN=YES \
-    -DCGNS_ENABLE_LFS=YES \
-    -DCGNS_ENABLE_TESTS=YES \
-    -DCGNS_BUILD_SHARED=YES \
-    -DCGNS_USE_SHARED=YES \
-
+    -DCGNS_ENABLE_FORTRAN=ON \
+    -DCGNS_ENABLE_HDF5=ON \
+    -DCGNS_ENABLE_TESTS=ON \
+    -DHDF5_NEED_ZLIB=ON \
+    -DZLIB_LIBRARY=$BUILD_PREFIX/lib/libz.so \
+    -DHDF5_m_LIBRARY_RELEASE=$BUILD_PREFIX/$HOST/sysroot/usr/lib/libm.so \
+    -DHDF5_rt_LIBRARY_RELEASE=$BUILD_PREFIX/$HOST/sysroot/usr/lib/librt.so
 make -j$CPU_COUNT
 # ctest --output-on-failure
 make install
