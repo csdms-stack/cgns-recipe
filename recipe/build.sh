f0754dcad0a3e1187b4eb9a7811f43aef8a1bc52
Mark Piper <mark.piper@colorado.edu>
2019-01-14 16:19:02 -0700

    Include CMake default options for clarity
    Add these options, even though they're defaults, just so that I know
    that they're set.

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,9 +3,12 @@
 mkdir _build && cd _build
 cmake .. \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCGNS_BUILD_SHARED=ON \
+    -DCGNS_USE_SHARED=ON \
     -DCGNS_ENABLE_FORTRAN=ON \
     -DCGNS_ENABLE_HDF5=ON \
     -DCGNS_ENABLE_TESTS=ON \
+    -DHDF5_NEED_SZIP=OFF \
     -DHDF5_NEED_ZLIB=ON \
     -DZLIB_LIBRARY=$BUILD_PREFIX/lib/libz.so \
     -DHDF5_m_LIBRARY_RELEASE=$BUILD_PREFIX/$HOST/sysroot/usr/lib/libm.so \
