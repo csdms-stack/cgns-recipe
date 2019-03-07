caa7410c64dc0aeeb6370c22ca99f1e724a50297
Mark Piper <mark.piper@colorado.edu>
2019-03-07 14:27:53 -0700

    Update for v3.2.1
    There were a few changes in cmake variables between v3.3.1 and v3.2.1.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,18 +1,8 @@
 #! /usr/bin/env bash
 
-_libpath=/usr/lib
-case `uname -s` in
-    Linux)
-	_libpath=$BUILD_PREFIX/$HOST/sysroot/usr/lib
-	;;
-    Darwin)
-	export MACOSX_DEPLOYMENT_TARGET=""
-	;;
-    *)
-	echo "Unknown OS"
-	exit 1
-esac
-export _libpath
+if [[ `uname -s` == 'Darwin' ]]; then
+    export MACOSX_DEPLOYMENT_TARGET=""
+fi
 
 mkdir _build && cd _build
 cmake .. \
@@ -21,17 +11,19 @@ cmake .. \
     -DCGNS_BUILD_SHARED=ON \
     -DCGNS_USE_SHARED=ON \
     -DCGNS_ENABLE_FORTRAN=ON \
-    -DCGNS_ENABLE_HDF5=ON \
+    -DFORTRAN_NAMING="LOWERCASE_" \
     -DCGNS_ENABLE_TESTS=ON \
     -DCGNS_ENABLE_LFS=ON \
     -DCMAKE_C_FLAGS:STRING=-D_LARGEFILE64_SOURCE \
+    -DCGNS_ENABLE_HDF5=ON \
+    -DHDF5_LIBRARY=$BUILD_PREFIX/lib/libhdf5$SHLIB_EXT \
+    -DHDF5_INCLUDE_PATH=$BUILD_PREFIX/include \
     -DHDF5_NEED_SZIP=OFF \
     -DHDF5_NEED_ZLIB=ON \
-    -DZLIB_LIBRARY=$BUILD_PREFIX/lib/libz$SHLIB_EXT \
-    -DHDF5_m_LIBRARY_RELEASE=$_libpath/libm$SHLIB_EXT \
-    -DHDF5_rt_LIBRARY_RELEASE=$_libpath/librt$SHLIB_EXT \
-    -DHDF5_dl_LIBRARY_RELEASE=$_libpath/libdl$SHLIB_EXT \
-    -DHDF5_pthread_LIBRARY_RELEASE=$_libpath/libpthread$SHLIB_EXT
+    -DZLIB_LIBRARY=$BUILD_PREFIX/lib/libz$SHLIB_EXT
+
+sed -i 's@^c@!c@' src/cgnslib_f.h  # check behavior on macOS
+
 make -j$CPU_COUNT
 if [[ `uname -s` == 'Linux' ]]; then
     ctest
