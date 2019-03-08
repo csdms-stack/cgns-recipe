4a1fb5df224dc18b90e8c581ca7e0d1183bcd68d
Mark Piper <mark.piper@colorado.edu>
2019-03-07 20:56:07 -0700

    Try building only static library on macOS

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,16 @@
 #! /usr/bin/env bash
 
+_build_shared=ON
 if [[ `uname -s` == 'Darwin' ]]; then
     export MACOSX_DEPLOYMENT_TARGET=""
+    _build_shared=OFF
 fi
 
 mkdir _build && cd _build
 cmake .. \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCGNS_BUILD_SHARED=ON \
+    -DCGNS_BUILD_SHARED=$_build_shared \
     -DCGNS_USE_SHARED=ON \
     -DCGNS_ENABLE_FORTRAN=ON \
     -DFORTRAN_NAMING="LOWERCASE_" \
